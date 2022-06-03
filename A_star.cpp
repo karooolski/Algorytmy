7fc63f83e894c8ffd972a26a3b560d5104feea1f
karooolski <106780820+karooolski@users.noreply.github.com>
2022-06-04 01:38:35 +0200

    Cosmetic changes
    - Now the list of Road coordinates is printed from first to the end

--- A_star.cpp
+++ A_star.cpp
@@ -9,7 +9,7 @@ struct Node {
 	int y;		// współrzędna y położenia węzła na płaszczyźnie
 	float distanceFromFirstNode;	// odległość przebyta od węzła startowego do tego węzła
 	float h;	// heurystyka - ile zostało w linii prostej do węzła końcowego
-	float sumDandH;	// suma d i h, używana do wybierania węzłów do sprawdzenia
+	float sum_D_H;	// suma d i h, używana do wybierania węzłów do sprawdzenia
 	int earlier_x;		// współrzędna x położenia węzła poprzedzającego bieżący na najkrótszej drodze
 	int earlier_y;		// współrzędna y położenia węzła poprzedzającego bieżący na najkrótszej drodze
 	bool nodeProcessed;		// czy węzeł został już sprawdzony?
@@ -22,30 +22,28 @@ struct Road {
 
 // Przeciążony operator porównania, używany podczas sortowania list
 bool operator < (Node a, Node b) {
-	if (!a.nodeProcessed && (a.sumDandH < b.sumDandH))
+	if (!a.nodeProcessed && (a.sum_D_H < b.sum_D_H))
 		return true;
 	return false;
 }
 
-void SetColor(int value) {
+void setColor(int value) {
 	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), value);
 }
 
 void green_collor() {
-	SetColor(10);
+	setColor(10);
 }
 void red_collor() {
-	SetColor(12);
+	setColor(12);
 	
 }
 void white_collor() {
-	SetColor(7);
+	setColor(7);
 }
 
-
-
 // Tylko sprawdzamy, czy węzeł o podanych współrzędnych znajduje się na wskazanej liście
-bool czy_jest(list<Node>* Q, int x, int y) {
+bool isNodeInList(list<Node>* Q, int x, int y) {
 	for (auto it = Q->begin(); it != Q->end(); it++) {
 		if (it->x == x)
 			if (it->y == y)
@@ -55,7 +53,7 @@ bool czy_jest(list<Node>* Q, int x, int y) {
 }
 
 // Szukamy węzeła o podanych współrzędnych na wskazanej liście i zwracamy do niego iterator
-bool find(list<Node>* toBeProcessed, int x, int y, list<Node>::iterator* wezel) {
+bool findNode(list<Node>* toBeProcessed, int x, int y, list<Node>::iterator* wezel) {
 	for (auto it = toBeProcessed->begin(); it != toBeProcessed->end(); it++) {
 		if (it->x == x)
 			if (it->y == y) {
@@ -78,86 +76,80 @@ bool is_xy_reachable(vector<vector<int>>* graf, int x, int y) {
 }
 
 // Funkcja licząca odległość między dwoma wskazanymi węzłami
-float h(int x, int y, int kx, int ky) {
-	return sqrt((kx - x) * (kx - x) + (ky - y) * (ky - y));
+// heurystka h
+float distanceBitweenNodes(int x, int y, int other_x, int other_y) {
+	return sqrt((other_x - x) * (other_x - x) + (other_y - y) * (other_y - y));
 }
 
 // Funkcja która ma dodać węzeł o podanych współrze∂nych do listy węzłów oczekujących na sprawdzenie
-// kx - x dolecelowe - last_x
-// ky - y docelowe   - last_y
-bool addNode(vector<vector<int>>* graf, int x, int y, int last_x, int last_y, int bx, int by, list<Node>* toBeProcessed, list<Node>* przetworzone) {
-	Node w;
+// kx - x dolecelowe -> last_x   // ky - y docelowe   -> last_y   // bx,by -> earlier_x, earlier_y
+bool addNode(vector<vector<int>>* graph, int x, int y, int last_x, int last_y, int earlier_x, int earlier_y, list<Node>* toBeProcessed, list<Node>* przetworzone) {
+	Node node;
 	list<Node>::iterator it;
 	float d = 0;
-	if (czy_jest(przetworzone, x, y))
+	if (isNodeInList(przetworzone, x, y))
 		return false;	// nie interesują nas węzły będące na liśczie przetworzonych
 	if (!toBeProcessed->empty())
 		d = toBeProcessed->front().distanceFromFirstNode;	// pobieramy drogę przebytą do węzła z którego tu trafiliśmy
-	if (is_xy_reachable(graf, x, y)) {
-		d = d + h(x, y, bx, by);	// obliczamy drogę do węzła który dodajemy przez węzeł z którego tu trafiliśmy
-		if (find(toBeProcessed, x, y, &it)) {	// sprawdzamy, czy dodawany węzeł nie jest aby już na liście do sprawdzenia
+	if (is_xy_reachable(graph, x, y)) {
+		d = d + distanceBitweenNodes(x, y, earlier_x, earlier_y);	// obliczamy drogę do węzła który dodajemy przez węzeł z którego tu trafiliśmy
+		if (findNode(toBeProcessed, x, y, &it)) {	// sprawdzamy, czy dodawany węzeł nie jest aby już na liście do sprawdzenia
 			if (d < it->distanceFromFirstNode) {	// relaksacja - sprawdzamy, czy aby nie znaleźliśmy lepszej (krótszej) drogi
 				it->distanceFromFirstNode = d;		// tak, znaleźliśmy,
-				it->sumDandH = d + it->h;	// więc wszystko przeliczamy
-				it->earlier_x = bx;	// i zazanaczemy, że prowadzi ona przez
-				it->earlier_y = by;	// inny węzeł, niż ta poprzednia droga
-
-				// dla wyswietlania drogi
-				graf->at(x).at(y) = 9;
+				it->sum_D_H = d + it->h;	// więc wszystko przeliczamy
+				it->earlier_x = earlier_x;	// i zazanaczemy, że prowadzi ona przez
+				it->earlier_y = earlier_y;	// inny węzeł, niż ta poprzednia droga
+				//graf->at(x).at(y) = 9; // dla wyswietlania drogi
 			}
 		}
 		else {	// ale jeśli tego węzła nie ma jeszcze na liście do sprawdzenia, to trzeba go dodać
-			w.x = x;
-			w.y = y;
-			w.distanceFromFirstNode = d;
-			w.h = h(x, y, last_x, last_y);
-			w.sumDandH = w.distanceFromFirstNode + w.h;
-			w.earlier_x = bx;
-			w.earlier_y = by;
-			w.nodeProcessed = false;
-			toBeProcessed->push_back(w);
+			node.x = x;
+			node.y = y;
+			node.distanceFromFirstNode = d;
+			node.h = distanceBitweenNodes(x, y, last_x, last_y);
+			node.sum_D_H = node.distanceFromFirstNode + node.h;
+			node.earlier_x = earlier_x;
+			node.earlier_y = earlier_y;
+			node.nodeProcessed = false;
+			toBeProcessed->push_back(node);
 		}
 		return true;
 	}
 	return false;
 }
 
-// sx sy - startowe wspolrzedne - start_x, start_y
-// kx ky - wspolrzedne dolecowe
+// sx sy - startowe wspolrzedne - start_x, start_y // kx ky - wspolrzedne dolecowe
 bool A_star(vector<vector<int>>* graph, int start_x, int start_y, int last_x, int last_y, list<Node>* toBeProcessed, list<Node>* processed) {
 	if (toBeProcessed->empty())	// czy mamy z czym pracować?
 		return false;	// Nieee... Tu nic nie ma... :(
 	list<Road> passedRoad;
 	// będzemy tak długo szukać, aż dojdziemy do węzła końcowego LUB lista Q będzie pusta
 	while (!((toBeProcessed->front().x == last_x && toBeProcessed->front().y == last_y) || toBeProcessed->empty())) {
-		addNode(graph, toBeProcessed->front().x - 1, toBeProcessed->front().y - 1, last_x, last_y, toBeProcessed->front().x, toBeProcessed->front().y, toBeProcessed, processed);
-		addNode(graph, toBeProcessed->front().x - 1, toBeProcessed->front().y, last_x, last_y, toBeProcessed->front().x, toBeProcessed->front().y, toBeProcessed, processed);
-		addNode(graph, toBeProcessed->front().x - 1, toBeProcessed->front().y + 1, last_x, last_y, toBeProcessed->front().x, toBeProcessed->front().y, toBeProcessed, processed);
-		addNode(graph, toBeProcessed->front().x, toBeProcessed->front().y - 1, last_x, last_y, toBeProcessed->front().x, toBeProcessed->front().y, toBeProcessed, processed);
-		addNode(graph, toBeProcessed->front().x, toBeProcessed->front().y + 1, last_x, last_y, toBeProcessed->front().x, toBeProcessed->front().y, toBeProcessed, processed);
-		addNode(graph, toBeProcessed->front().x + 1, toBeProcessed->front().y - 1, last_x, last_y, toBeProcessed->front().x, toBeProcessed->front().y, toBeProcessed, processed);
-		addNode(graph, toBeProcessed->front().x + 1, toBeProcessed->front().y, last_x, last_y, toBeProcessed->front().x, toBeProcessed->front().y, toBeProcessed, processed);
-		addNode(graph, toBeProcessed->front().x + 1, toBeProcessed->front().y + 1, last_x, last_y, toBeProcessed->front().x, toBeProcessed->front().y, toBeProcessed, processed);
-
+		addNode(graph, toBeProcessed->front().x - 1,toBeProcessed->front().y - 1,	last_x, last_y, toBeProcessed->front().x, toBeProcessed->front().y, toBeProcessed, processed);
+		addNode(graph, toBeProcessed->front().x - 1,toBeProcessed->front().y,		last_x, last_y, toBeProcessed->front().x, toBeProcessed->front().y, toBeProcessed, processed);
+		addNode(graph, toBeProcessed->front().x - 1,toBeProcessed->front().y + 1,	last_x, last_y, toBeProcessed->front().x, toBeProcessed->front().y, toBeProcessed, processed);
+		addNode(graph, toBeProcessed->front().x,	toBeProcessed->front().y - 1,	last_x, last_y, toBeProcessed->front().x, toBeProcessed->front().y, toBeProcessed, processed);
+		addNode(graph, toBeProcessed->front().x,	toBeProcessed->front().y + 1,	last_x, last_y, toBeProcessed->front().x, toBeProcessed->front().y, toBeProcessed, processed);
+		addNode(graph, toBeProcessed->front().x + 1,toBeProcessed->front().y - 1,	last_x, last_y, toBeProcessed->front().x, toBeProcessed->front().y, toBeProcessed, processed);
+		addNode(graph, toBeProcessed->front().x + 1,toBeProcessed->front().y,		last_x, last_y, toBeProcessed->front().x, toBeProcessed->front().y, toBeProcessed, processed);
+		addNode(graph, toBeProcessed->front().x + 1,toBeProcessed->front().y + 1,	last_x, last_y, toBeProcessed->front().x, toBeProcessed->front().y, toBeProcessed, processed);
 		toBeProcessed->front().nodeProcessed = true;
 		processed->push_back(toBeProcessed->front());
 		toBeProcessed->pop_front();
 		toBeProcessed->sort();
-
-		// tym zobaczysz wszystkie przetworzone 
-		//graf->at(do_przetworzenia->front().x).at(do_przetworzenia->front().y) = 3;
+		//graf->at(toBeProcessed->front().x).at(toBeProcessed->front().y) = 3; // tym zobaczysz wszystkie przetworzone 
 	}
 	if (toBeProcessed->empty())	// lista Q jest pusta, więc nie znaleźliśmy drogi...
 		return false;
 	processed->push_back(toBeProcessed->front());	// droga została znaleziona, więc dodajmy węzeł końcowy do listy sprzwdzonych
 
+	// finding last node coordinates
 	list<Node>::iterator toLastNode;
-
 	for (toLastNode = processed->begin(); toLastNode != processed->end(); ) {
 		toLastNode++;
 	}
 	toLastNode--; // found x,y of last node 
-	passedRoad.push_back({ toLastNode->x, toLastNode->y });
+	passedRoad.push_front({ toLastNode->x, toLastNode->y });
 	int temp_x = last_x;
 	int temp_y = last_y;
 	list<Node>::iterator toFirstNode = toLastNode;
@@ -167,11 +159,11 @@ bool A_star(vector<vector<int>>* graph, int start_x, int start_y, int last_x, in
 		int earlier_y = toFirstNode->earlier_y;
 		temp_x = earlier_x;
 		temp_y = earlier_y;
-		passedRoad.push_back({ earlier_x, earlier_y });
-		// find one node before
+		passedRoad.push_front({ earlier_x, earlier_y });
+		// find a node before, we know its x,y, but need an object to find another node
 		for (list<Node>::iterator itt = processed->begin(); itt != processed->end(); itt++) {
 			if (itt->x == earlier_x && itt->y == earlier_y) {
-				toFirstNode = itt;
+				toFirstNode = itt; // found node before
 			}
 		}
 	}
@@ -179,10 +171,13 @@ bool A_star(vector<vector<int>>* graph, int start_x, int start_y, int last_x, in
 	for (list<Road>::iterator markNode = passedRoad.begin(); markNode != passedRoad.end(); markNode++) {
 		graph->at(markNode->x).at(markNode->y) = 3;
 	}
-	//show road parameters (x,y of each road Node) 
+	cout << "\n";
+	//show road parameters (x,y of each road Node) from end do start
+	cout << "poczatek\n";
 	for (list<Road>::iterator it = passedRoad.begin(); it != passedRoad.end(); it++) {
 		cout << "(" << it->x << "," << it->y << ")" << endl;
 	}
+	cout << "koniec\n";
 	return true;
 }
 
@@ -272,4 +267,3 @@ int main()
 
 	return 0;
 }
-
